5bffe9543ec1879287e519e570aff933eea8a5d5
Vadim Zaliva <lord@crocodile.org>
2019-09-11 07:56:26 -0700

    fixed typos and grammar mistakes

--- asn_strtoimax_lim.c
+++ asn_strtoimax_lim.c
@@ -9,17 +9,14 @@
 
 	if(str >= *end) return ASN_STRTOX_ERROR_INVAL;
 
-	switch(*str) {
-	case '-':
-	    last_digit_max++;
-	    sign = -1;
-	    /* FALL THROUGH */
-	case '+':
-	    str++;
-	    if(str >= *end) {
-		*end = str;
-		return ASN_STRTOX_EXPECT_MORE; }
-	}
+	switch(*str) { case '-':
+	               last_digit_max++;
+		       sign = -1;
+		       /* FALL THROUGH */
+	               case '+':
+			 str++;
+			 if(str >= *end) { *end = str;
+			                   return ASN_STRTOX_EXPECT_MORE; }}
 
 	for(value = 0; str < (*end); str++) {
 	    if(*str >= 0x30 && *str <= 0x39) {
@@ -28,39 +25,24 @@
 		    value = value * 10 + d;
 		} else if(value == upper_boundary) {
 		    if(d <= last_digit_max) {
-			if(sign > 0) {
-			    value = value * 10 + d;
-			} else {
-			    sign = 1;
-			    value = -value * 10 - d;
-			}
+			if(sign > 0) { value = value * 10 + d;
+			} else { sign = 1;
+			         value = -value * 10 - d; }
 			str += 1;
 			if(str < *end) {
 			    // If digits continue, we're guaranteed out of range.
 			    *end = str;
-			    if(*str >= 0x30 && *str <= 0x39) {
-				return ASN_STRTOX_ERROR_RANGE;
-			    } else {
-				*intp = sign * value;
-				return ASN_STRTOX_EXTRA_DATA;
-			    }
-			}
+			    if(*str >= 0x30 && *str <= 0x39) { return ASN_STRTOX_ERROR_RANGE;
+			    } else { *intp = sign * value;
+				      return ASN_STRTOX_EXTRA_DATA; }}
 			break;
-		    } else {
-			*end = str;
-			return ASN_STRTOX_ERROR_RANGE;
-		    }
-		} else {
-		    *end = str;
-		    return ASN_STRTOX_ERROR_RANGE;
-		}
-	    } else {
-		*end = str;
-		*intp = sign * value;
-		return ASN_STRTOX_EXTRA_DATA;
-	    }
-	}
-
+		    } else { *end = str;
+			     return ASN_STRTOX_ERROR_RANGE; }
+		} else { *end = str;
+		         return ASN_STRTOX_ERROR_RANGE; }
+	    } else { *end = str;
+		     *intp = sign * value;
+		     return ASN_STRTOX_EXTRA_DATA; }}
 	*end = str;
 	*intp = sign * value;
 	return ASN_STRTOX_OK; }
--- asn_strtoimax_lim_old.c
+++ asn_strtoimax_lim_old.c
@@ -11,18 +11,15 @@
 
 	    if(str >= *end) return ASN_STRTOX_ERROR_INVAL;
 
-	    switch(*str) {
-	    case '-':
-		    last_digit_max++;
-		    sign = -1;
-		    /* FALL THROUGH */
-	    case '+':
-		    str++;
-		    if(str >= *end) {
-			    *end = str;
-			    return ASN_STRTOX_EXPECT_MORE;
-		    }
-	    }
+	    switch(*str) { case '-':
+		           last_digit_max++;
+			   sign = -1;
+			   /* FALL THROUGH */
+	                   case '+':
+			     str++;
+			     if(str >= *end) {
+			       *end = str;
+			       return ASN_STRTOX_EXPECT_MORE; } }
 
 	    for(value = 0; str < (*end); str++) {
 		    switch(*str) {
@@ -33,30 +30,18 @@
 				    value = value * 10 + d;
 			    } else if(value == upper_boundary) {
 				    if(d <= last_digit_max) {
-					    if(sign > 0) {
-						    value = value * 10 + d;
-					    } else {
-						    sign = 1;
-						    value = -value * 10 - d;
-					    }
-				    } else {
-					    *end = str;
-					    return ASN_STRTOX_ERROR_RANGE;
-				    }
-			    } else {
-				    *end = str;
-				    return ASN_STRTOX_ERROR_RANGE;
-			    }
-			}
+					    if(sign > 0) { value = value * 10 + d;
+					    } else { sign = 1;
+						     value = -value * 10 - d; }
+				    } else { *end = str;
+					     return ASN_STRTOX_ERROR_RANGE; }
+			    } else { *end = str;
+				     return ASN_STRTOX_ERROR_RANGE; } }
 			continue;
 		    default:
 			*end = str;
 			*intp = sign * value;
-			return ASN_STRTOX_EXTRA_DATA;
-		    }
-	    }
-
+			return ASN_STRTOX_EXTRA_DATA; } }
 	    *end = str;
 	    *intp = sign * value;
-	    return ASN_STRTOX_OK;
-    }
+	    return ASN_STRTOX_OK; }
